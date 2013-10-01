869ac608e3e70ba7555bde64be151d482eafd669
Progi1984 <progi1984@gmail.com>
2013-10-01 16:17:51 +0200

    Support des fonctions de YATS en C

---
+++ src/YATSC.php
@@ -0,0 +1,56 @@
+<?php
+if(!extension_loaded('yats')){
+  require 'YATSPHP.class.php';
+
+  /**
+   * @param string $filename
+   * @param string docroot
+   * @param string searchpath
+   * @return YATSPHP
+   */
+  function yats_define($filename, $docroot = null, $searchpath = null){
+    $oYATS = new YATSPHP();
+    $oYATS->define($filename, $docroot);
+    return $oYATS;
+  }
+  /**
+   *
+   * @param YATSPHP $template
+   * @param string|array $var
+   * @param  $value
+   * @return boolean
+   */
+  function yats_assign($template, $var, $value = null){
+    return $template->assign($var, $value);
+  }
+
+  /**
+   * @param YATSPHP $template
+   * @param string $locale
+   * @param string $gettext_domain
+   * @param  string $gettext_dir
+   * @return string
+   */
+  function yats_getbuf($template, $locale = null, $gettext_domain = null , $gettext_dir = null){
+    return $template->render();
+  }
+
+  /**
+   * @param YATSPHP $template
+   * @param string $section
+   * @param boolean $hide
+   * @param integer $row
+   * @return boolean
+   */
+  function yats_hide($template, $section, $hide, $row = null){
+    return $template->hide($section, $hide, $row);
+  }
+
+  /**
+   * @param YATSPHP $template
+   * @return array
+   */
+  function yats_getvars($template){
+    //return $template
+  }
+}
