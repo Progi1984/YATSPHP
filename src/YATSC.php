44708c340c046ff9e7ee97dd8042de0e6b55fa2d
Progi1984 <progi1984@gmail.com>
2013-12-26 11:39:39 +0100

    IMPROVED : PHPCodeStyle

--- src/YATSC.php
+++ src/YATSC.php
@@ -1,55 +1,60 @@
 <?php
-if(!extension_loaded('yats')){
-  /**
-   * @param string $filename
-   * @param string docroot
-   * @param string searchpath
-   * @return YATSPHP
-   */
-  function yats_define($filename, $docroot = null, $searchpath = null){
-    $oYATS = new YATSPHP();
-    $oYATS->define($filename, $docroot, $searchpath);
-    return $oYATS;
-  }
+if (!extension_loaded('yats')) {
+    /**
+     * @param string $filename
+     * @param string docroot
+     * @param string searchpath
+     * @return YATSPHP
+     */
+    function yats_define($filename, $docroot = null, $searchpath = null)
+    {
+      $oYATS = new YATSPHP();
+      $oYATS->define($filename, $docroot, $searchpath);
+      return $oYATS;
+    }
 
-  /**
-   *
-   * @param YATSPHP $template
-   * @param string|array $var
-   * @param  $value
-   * @return boolean
-   */
-  function yats_assign($template, $var, $value = null){
-    return $template->assign($var, $value);
-  }
+    /**
+     *
+     * @param YATSPHP $template
+     * @param string|array $var
+     * @param  $value
+     * @return boolean
+     */
+    function yats_assign($template, $var, $value = null)
+    {
+      return $template->assign($var, $value);
+    }
 
-  /**
-   * @param YATSPHP $template
-   * @param string $locale
-   * @param string $gettext_domain
-   * @param  string $gettext_dir
-   * @return string
-   */
-  function yats_getbuf($template, $locale = null, $gettext_domain = null , $gettext_dir = null){
-    return $template->render();
-  }
+    /**
+     * @param YATSPHP $template
+     * @param string $locale
+     * @param string $gettext_domain
+     * @param string $gettext_dir
+     * @return string
+     */
+    function yats_getbuf($template, $locale = null, $gettext_domain = null, $gettext_dir = null)
+    {
+      return $template->render();
+    }
 
-  /**
-   * @param YATSPHP $template
-   * @param string $section
-   * @param boolean $hide
-   * @param integer $row
-   * @return boolean
-   */
-  function yats_hide($template, $section, $hide, $row = null){
-    return $template->hide($section, $hide, $row);
-  }
+    /**
+     * @param YATSPHP $template
+     * @param string $section
+     * @param boolean $hide
+     * @param integer $row
+     * @return boolean
+     */
+    function yats_hide($template, $section, $hide, $row = null)
+    {
+      return $template->hide($section, $hide, $row);
+    }
 
-  /**
-   * @param YATSPHP $template
-   * @return array
-   */
-  function yats_getvars($template){
-    //return $template
-  }
+    /**
+     * @param YATSPHP $template
+     * @return array
+     */
+    function yats_getvars($template)
+    {
+      //return $template
+    }
 }
