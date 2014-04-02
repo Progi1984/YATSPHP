7cd86fdaa1c9bfe4045b8f11ad4b48f294ee3c91
Progi1984 <progi1984@gmail.com>
2014-04-02 23:16:35 +0200

    ADDED : Unit Tests with generation of Clover report to Coveralls.io

---
+++ .atoum.php
@@ -0,0 +1,11 @@
+<?php
+
+  use \mageekguy\atoum;
+
+  $script->addDefaultReport();
+
+  $cloverWriter = new atoum\writers\file('./build/logs/clover.xml');
+  $cloverReport = new atoum\reports\asynchronous\clover();
+  $cloverReport->addWriter($cloverWriter);
+
+  $runner->addReport($cloverReport);
