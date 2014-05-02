0f70abdf94d8b296a698142c32a93633cf2fd6bc
Progi1984 <progi1984@gmail.com>
2014-05-02 20:02:13 +0200

    IMPROVED : Génération de la PHPDoc

---
+++ .travis_shell_after_success.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ "$TRAVIS_REPO_SLUG" == "Progi1984/YATSPHP" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+
+  echo -e "Publishing PHPDoc...\n"
+
+  cp -R build/docs $HOME/docs-latest
+
+  cd $HOME
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "travis-ci"
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/Progi1984/YATSPHP gh-pages > /dev/null
+
+  cd gh-pages
+  cd docs
+  git rm -rf ./$TRAVIS_BRANCH
+  cp -Rf $HOME/docs-latest ./$TRAVIS_BRANCH
+  git add -f .
+  git commit -m "PHPDocumentor (Travis Build : $TRAVIS_BUILD_NUMBER  - Branch : $TRAVIS_BRANCH)"
+  git push -fq origin gh-pages > /dev/null
+
+  echo -e "Published PHPDoc to gh-pages.\n"
+
+fi
